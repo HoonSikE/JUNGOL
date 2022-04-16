60563fef9743494fd749d5e7ee8568206fa293ea
HoonSikE <hoongrammer@gmail.com>
2022-04-16 16:12:01 +0900

    Update Jungol_111_연산자_형성평가1.java

--- src/Language_Coder_03_연산자/Jungol_111_연산자_형성평가1.java
+++ src/Language_Coder_03_연산자/Jungol_111_연산자_형성평가1.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * @author HanHoon
+ * http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=155&sca=1030
+ */
 public class Jungol_111_연산자_형성평가1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
